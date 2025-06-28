d1cc0b70f45f50dd4f15759361ca2eccbc1e5781
igr47 <njagielvis44@gmail.com>
2025-06-28 15:48:38 +0300

    The firs step

---
+++ fileSaver.h
@@ -0,0 +1,14 @@
+#ifdef NETWORK_H
+#define NETWORK_H
+#include <nlohmann/json.hpp>
+using json=nlohmann::json;
+
+class toJsonUtility{
+        public:
+                template<typename T>
+                static json toJsonCollection(std::vector<std::shared_ptr<T>>& items);
+                void writeToFile(const std::string& filename,const json& data);
+                template<typename T>
+                static std::vector<std::shared_ptr<T>> readCollectionFromFile(const std::string& filename);
+}
+#endif
